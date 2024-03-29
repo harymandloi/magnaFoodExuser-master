b4e5d5b6eb464975a4b28af2a67c6c656d1c81e8
Hariom <mandaloihariom@gmail.com>
2024-03-29 10:33:42 +0530

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,21 @@
+final String appName = 'Magna Food Ex';
+
+final String packageName = 'com.magnafoodex.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.magnafoodex.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.magnafoodex.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://buransmart.in/app/v1/api/';
+final String baseUrl = 'https://developmentalphawizz.com/jmd_ganaadi/app/v1/api/';
+
+final String imageUrl = 'https://developmentalphawizz.com/jmd_ganaadi/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
